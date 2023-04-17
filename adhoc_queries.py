458103da3d04fdd443e1fa8b3999efb0913f5314
shaneorourke <shaneorourke110@gmail.com>
2023-04-17 16:33:58 +0100

    Update adhoc_queries.py

--- adhoc_queries.py
+++ adhoc_queries.py
@@ -1,13 +1,13 @@
 import pandas as pd
 import sqlite3 as sql
 
-tp = 24.294 + (24.294 * 0.05)
 
 conn = sql.connect('bybit_sma')
 cur = conn.cursor()
-cur.execute('''
-        UPDATE Logs
-        set take_profit = 25.509
-        where order_id = "d64dc678-e115-45db-bcf2-4e5dd5b57e39" and log_type != "log"
-        ''')
-conn.commit()
+logs = pd.read_sql('''
+        SELECT id,log_type,order_id,close,fast_sma,slow_sma,cross,last_cross,market_date,buy_sell 
+        FROM Logs
+        ORDER BY id DESC
+        ''',conn)
+
+print(logs)
