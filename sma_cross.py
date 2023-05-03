a99758186d96b4e4f340660c947f06e75c1c4351
shaneorourke <shaneorourke110@gmail.com>
2023-05-03 15:16:35 +0100

    Update sma_cross.py

--- sma_cross.py
+++ sma_cross.py
@@ -241,7 +241,7 @@ if __name__ == '__main__':
 
     open_position = check_open_position()
     if not open_position > 0.0:
-        sma_cross_entry_strategy(candles,trading_symbol,0.05)
+        sma_cross_entry_strategy(candles,trading_symbol,0.025)
     else:
         sma_cross_exit_strategy(candles,trading_symbol)
 
